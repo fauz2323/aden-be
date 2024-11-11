bcb7cde4192f12b4e94cd5ccd6cd0bb7e56fdb63
Zen <59063107+fauz2323@users.noreply.github.com>
2024-11-11 19:35:32 +0700

    add

---
+++ database/seeders/InitialSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class InitialSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'phone'=>00000000,
+            'type'=>'admin',
+            'password'=>Hash::make('admin')
+        ]);
+    }
+}
